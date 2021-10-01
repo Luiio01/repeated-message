0ece38239ada87974f85b93fd4c31ae6b0e31254
Luiio01 <91396155+Luiio01@users.noreply.github.com>
2021-10-01 18:42:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,28 @@
-import telebot
-import config
+import time
+import schedule
+import requests
 
 
-bot = telebot.TeleBot(config. TOKEN)
-@bot.message_handler(content_types=['text'])
-def lalala(message):
+def telegram_bot_sendtext(bot_message):
     
- bot.send_message(message.chat.id, message.text)
-#RUN
-bot.polling(none_stop=True)
+    bot_token = ''
+    bot_chatID = ''
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+
+def report():
+    my_balance = 10   ## Replace this number with an API call to fetch your account balance
+    my_message = "Current balance is: {}".format(my_balance)   ## Customize your message
+    telegram_bot_sendtext(my_message)
+
+
+    
+schedule.every().day.at("12:00").do(report)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
